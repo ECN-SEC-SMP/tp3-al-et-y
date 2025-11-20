bb993a87dfecd6881d75157ec4a9eec26e77a73d
AL <stephantandrelouis@gmail.com>
2025-11-20 14:40:46 +0100

    Création dossier build

--- Rectangle.hpp
+++ Rectangle.hpp
@@ -3,7 +3,7 @@
 #include "PointT.hpp"
 #include "Forme.hpp"
 #include <iostream>
-
+#include <array>
 
 template <typename T>
 class Rectangle;
@@ -16,8 +16,35 @@ template <typename T>
 class Rectangle : public Forme<T>
 {
 public:
-    //Constructeur
-    Rectangle()
+    // Constructeur
+    Rectangle();
+
+    // Accesseurs
+    T getH() const { return this->h; }
+    T getL() const { return this->l; }
+
+    // Mutateurs
+    T setH(T h) { this->h = h; }
+    T setL(T l) { this->l = l; }
+
+    // Méthodes
+    template <typename TPerimetre>
+    std::array<PointT<T>, 4> vertices() const
+    {
+        T x0 = this->getPoint().getX();
+        T y0 = this->getPoint().getY();
+        T x1 = x0 + l;
+        T y1 = y0 + h;
+
+        return {PointT<T>(x0, y0),
+                PointT<T>(x1, y0),
+                PointT<T>(x1, y1),
+                PointT<T>(x0, y1)};
+    }
+
+private:
+    T h; // Hauteur du rectangle
+    T l; // Largeur du rectangle
 };
 
 template <typename T>
--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include "Forme.hpp"
+#include "PointT.hpp"
+#include "Rectangle.hpp"
 
 int main()
 {
